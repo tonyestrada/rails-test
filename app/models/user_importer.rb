7d7dcfbbb57601a58fe6dc2b87b2a4cd8c01be9a
Kyle Swankie <kyle.swankie@vitals.com>
2017-06-20 17:12:39 -0400

    add csv for each method import

--- app/models/user_importer.rb
+++ app/models/user_importer.rb
@@ -1,7 +1,9 @@
+require 'csv'
+
 class UserImporter
   def self.import!(csv_path)
-    3.times do
-      User.create(first_name: 'Freddy', last_name: 'Mercurie')
+    CSV.foreach(csv_path) do |row|
+      binding.pry
     end
   end
 end
